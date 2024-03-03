ff66b53309bb1b36866a4de9267de24b4e30b806
NeerajSharma10 <neerajsh1012@gmail.com>
2024-03-03 16:22:55 +0530

    added zipkin configuration

--- order-service/src/main/java/com/njcoder/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/njcoder/orderservice/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.njcoder.orderservice.service.InventoryCaller;
 import com.njcoder.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
@@ -20,8 +22,10 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     private String createOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("****************Welcome to OrderService**********************");
         Boolean isProductAvailableInStock = orderService.createOrder(orderRequest);
         if (isProductAvailableInStock){
+            log.info("****************End to OrderService**********************");
             return "Order created successfully";
         }
         return "Order not created successfully";
