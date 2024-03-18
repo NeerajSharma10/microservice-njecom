af9fa57c0f8b79f33b7607c0703e4ec0f197cd1a
NeerajSharma10 <neerajsh1012@gmail.com>
2024-03-18 21:18:25 +0530

    added code with updation in api gateway

--- api-gateway/src/main/java/com/njcoder/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/njcoder/apigateway/config/SecurityConfig.java
@@ -11,37 +11,35 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
-@Slf4j
 public class SecurityConfig {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer.uri}")
-    private String issuerUri;
-
     @Bean
-    public ReactiveJwtDecoder reactiveJwtDecoder() {
-        // Customize this method based on your JWT decoding requirements
-        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuerUri);
-    }
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .authorizeExchange(exchange ->
+//                        exchange.pathMatchers("/eureka/**")
+//                                .permitAll()
+//                                .anyExchange()
+//                                .authenticated())
+//                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
+//        return serverHttpSecurity.build();
+        serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
+        return serverHttpSecurity.build();
 
-    @Bean
-    public SecurityFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-//        log.info("issuer Uri Value {} : ",issuerUri);
-        System.out.println("hello this -------------------------------- : " + issuerUri);
-        serverHttpSecurity
-                .csrf(csrfSpec -> csrfSpec.disable())
-                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
-                        .permitAll()
-                        .anyExchange()
-                        .authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtDecoder(reactiveJwtDecoder())))
-                .oauth2Login(withDefaults());
-        return (SecurityFilterChain) serverHttpSecurity.build();
     }
-
 }
